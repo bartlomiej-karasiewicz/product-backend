ee770b2edfbaa29172076341426fe07c5a818442
bkarasiewicz <bkarasiewicz@pzu.pl>
2024-06-19 17:08:28 +0200

    Update cors mapping

--- src/main/java/com/task/productbackend/configuration/WebConfig.java
+++ src/main/java/com/task/productbackend/configuration/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/products/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
